b1c1659130880249470964ff389dbae373ab0242
Damir Sagalbaev <105151275+s1lda@users.noreply.github.com>
2023-03-20 20:17:14 +0600

    Add files via upload

---
+++ tau/ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tau
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите фразу: ");
+            string[] text = Console.ReadLine().Split(' ');
+            string[] tmp = new string[text.Length];
+
+            int k = text.Length / 2;
+            for (int i = 0; i < text.Length; i++)
+            {
+                k += Convert.ToInt32(Math.Pow(-1, i) * i);
+                tmp[i] = Convert_Word(text[k]);
+            }
+            Console.WriteLine(String.Join(" ", tmp));
+            Console.ReadKey();
+        }
+        static string Convert_Word(string word)
+        {
+            char[] tmp = new char[word.Length];
+            int k = word.Length / 2;
+            for (int i = 0; i < word.Length; i++)
+            {
+                k += Convert.ToInt32(Math.Pow(-1, i) * i);
+                tmp[i] = word[k];
+            }
+            return new string(tmp);
+        }
+    }
+}
